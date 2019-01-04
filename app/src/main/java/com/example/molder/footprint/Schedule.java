9a2af44f01409ade9bd128984d7182026a8b4fcc
lulu <weidaily303@gmail.com>
2019-01-04 15:34:15 +0800

    test3

--- app/src/main/java/com/example/molder/footprint/Schedule.java
+++ app/src/main/java/com/example/molder/footprint/Schedule.java
@@ -3,11 +3,11 @@ package com.example.molder.footprint;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class Stroke extends AppCompatActivity {
+public class Schedule extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_stroke);
+        setContentView(R.layout.schedule_main);
     }
 }
