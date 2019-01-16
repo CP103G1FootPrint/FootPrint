0dd39e8deeb2b016f3950d538630be0e42811b00
lulu <weidaily303@gmail.com>
2019-01-16 15:31:34 +0800

    TEST0116

--- app/src/main/java/com/example/molder/footprint/Home.java
+++ app/src/main/java/com/example/molder/footprint/Home.java
@@ -3,16 +3,11 @@ package com.example.molder.footprint;
 import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
-import android.support.design.widget.TabItem;
-import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
-import android.support.v4.view.PagerAdapter;
-import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 
 
@@ -39,7 +34,7 @@ public class Home extends AppCompatActivity {
                 Fragment fragment;
                 switch (item.getItemId()) {
                     case R.id.homeStrokeBtnNavigation:
-                        fragment = new ScheduleFragment();
+                        fragment = new ScheduleMainFragment();
                         break;
                     case R.id.homeTakePictureBtnNavigation:
                         fragment = new HomeFragment();
