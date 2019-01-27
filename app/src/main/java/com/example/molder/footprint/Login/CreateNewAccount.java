3e3d24aa13fde0b0d0fbb8a7b31482c65c0ef38e
molder <molder1007@gmail.com>
2019-01-27 21:52:43 +0800

    20180127 1.串接server 2.串接db 3.checkInShare, map, login

--- app/src/main/java/com/example/molder/footprint/Login/CreateNewAccount.java
+++ app/src/main/java/com/example/molder/footprint/Login/CreateNewAccount.java
@@ -1,24 +1,36 @@
 package com.example.molder.footprint.Login;
 
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.Toast;
 
+import com.example.molder.footprint.Common.Common;
+import com.example.molder.footprint.Common.CommonTask;
 import com.example.molder.footprint.R;
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+
+
 
 public class CreateNewAccount extends AppCompatActivity {
 
+    private static final String TAG = "CreateNewAccount";
     private TextInputLayout createAccount;
     private TextInputLayout createPassword;
     private EditText createNickName;
     private EditText createBirthday;
     private Spinner createSpConstellation;
+    private boolean accountExist = false;
+    private CommonTask accountExistTask;
+    private String text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,6 +55,9 @@ public class CreateNewAccount extends AppCompatActivity {
         @Override
         public void onItemSelected(
                 AdapterView<?> parent, View view, int pos, long id) {
+            Object item = createSpConstellation.getSelectedItem();
+            String category = item.toString().trim();
+            showToast(CreateNewAccount.this,category);
 
         }
 
@@ -58,12 +73,34 @@ public class CreateNewAccount extends AppCompatActivity {
         String pattern = "^.*@gmail\\.com$";
 
         if(emailInput.isEmpty() ){
-            createAccount.setError("Field can't be empty");
+            createAccount.setError("未填");
             return false;
         }else if(!emailInput.matches(pattern)){
-            createAccount.setError("Not a valid Email");
+            createAccount.setError("無效帳號");
             return false;
-        }else {
+        }if (Common.networkConnected(this)) {
+            String url = Common.URL + "/AccountServlet";
+            JsonObject jsonObject = new JsonObject();
+            jsonObject.addProperty("action", "accountExist");
+            jsonObject.addProperty("userId", emailInput);
+            String jsonOut = jsonObject.toString();
+            accountExistTask = new CommonTask(url, jsonOut);
+            try {
+                String result = accountExistTask.execute().get();
+                accountExist = Boolean.valueOf(result);
+            } catch (Exception e) {
+                Log.e(TAG, e.toString());
+            }
+            // show an error message if the id exists;
+            // otherwise, the error message should be clear
+            if (accountExist) {
+                createAccount.setError("帳號已存在");
+                return false;
+            } else {
+                createAccount.setError(null);
+                return true;
+            }
+        } else {
             //doSomething
             createAccount.setError(null);
             return true;
@@ -75,7 +112,7 @@ public class CreateNewAccount extends AppCompatActivity {
         String passwordInput = createPassword.getEditText().getText().toString().trim();
 
         if(passwordInput.isEmpty()){
-            createPassword.setError("Field can't be empty");
+            createPassword.setError("未填");
             return false;
         }else {
             createPassword.setError(null);
@@ -88,7 +125,7 @@ public class CreateNewAccount extends AppCompatActivity {
         String nickName = createNickName.getText().toString().trim();
 
         if(nickName.isEmpty()){
-            createNickName.setError("Field can't be empty");
+            createNickName.setError("未填");
             return false;
         }else {
             createNickName.setError(null);
@@ -102,10 +139,10 @@ public class CreateNewAccount extends AppCompatActivity {
         String pattern = "^(0?[1-9]|1[0-2])/(0?[1-9]|1\\d|2\\d|3[0-1])$";
 
         if(birthday.isEmpty()){
-            createBirthday.setError("Field can't be empty");
+            createBirthday.setError("未填");
             return false;
         }else if(!birthday.matches(pattern)){
-            createBirthday.setError("Not a valid Birthday");
+            createBirthday.setError("無效生日");
             return false;
             }else {
             //doSomething
@@ -117,9 +154,49 @@ public class CreateNewAccount extends AppCompatActivity {
     //確認按鈕 當以上檢查都成立時會送出
     public void createAccountConfirm(View view){
         if(validateEmail() && validatePassword() && validateNickName() && validateBirthday()){
-            Toast.makeText(this,"Create Account Success",Toast.LENGTH_LONG).show();
-            finish();
-//            return ;
+
+            String emailInput = createAccount.getEditText().getText().toString().trim();
+            String passwordInput = createPassword.getEditText().getText().toString().trim();
+            String nickName = createNickName.getText().toString().trim();
+            String birthday = createBirthday.getText().toString().trim();
+            Object item = createSpConstellation.getSelectedItem();
+            String category = item.toString().trim();
+            int integral = 0;
+
+            if (Common.networkConnected(this)) {
+                String url = Common.URL + "/AccountServlet";
+                Account account = new Account(emailInput, passwordInput, nickName, birthday, category,integral);
+                JsonObject jsonObject = new JsonObject();
+                //新增
+                jsonObject.addProperty("action", "accountInsert");
+                //地址 電話
+                jsonObject.addProperty("account", new Gson().toJson(account));
+                int count = 0;
+                try {
+                    String result = new CommonTask(url, jsonObject.toString()).execute().get();
+                    count = Integer.valueOf(result);
+
+                } catch (Exception e) {
+                    Log.e(TAG, e.toString());
+                }
+                if (count == 0) {
+                    Common.showToast(this, R.string.msg_InsertFail);
+                } else {
+                    // user ID and password will be saved in the preferences file
+                    // and starts UserActivity
+                    // while the user account is created successfully
+                    SharedPreferences preferences = getSharedPreferences(
+                            Common.PREF_FILE, MODE_PRIVATE);
+                    preferences.edit().putBoolean("login", true)
+                            .putString("userId", emailInput)
+                            .putString("password", passwordInput).apply();
+                    Common.showToast(this, R.string.msg_InsertSuccess);
+                    finish();
+                }
+            } else {
+                Common.showToast(this, R.string.msg_NoNetwork);
+            }
+
         }
 
     }
@@ -128,18 +205,18 @@ public class CreateNewAccount extends AppCompatActivity {
     public void createAccountCancel(View view){
         finish();
     }
-//    public Boolean checkRule(String x,Object error,String pattern){
-//        if(x.isEmpty()){
-//            error.setError("Field can't be empty");
-//            return false;
-//        }else if(!x.matches(pattern)){
-//            error.setError("Not a valid Birthday");
-//            return false;
-//        }else {
-//            //doSomething
-//            error.setError(null);
-//            return true;
-//
-//        }
-//    }
+
+
+    public static void showToast(Context context, String message) {
+        Toast.makeText(context, message, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (accountExistTask != null) {
+            accountExistTask.cancel(true);
+            accountExistTask = null;
+        }
+    }
 }
---
+++ app/src/main/java/com/example/molder/footprint/Map/LandMarkInfo.java
@@ -0,0 +1,284 @@
+package com.example.molder.footprint.Map;
+
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.StaggeredGridLayoutManager;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.RatingBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.molder.footprint.CheckInShare.Picture;
+import com.example.molder.footprint.Common.Common;
+import com.example.molder.footprint.Common.CommonTask;
+import com.example.molder.footprint.Common.ImageTask;
+import com.example.molder.footprint.R;
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import com.google.gson.reflect.TypeToken;
+
+import java.lang.reflect.Type;
+import java.util.List;
+
+public class LandMarkInfo extends AppCompatActivity {
+
+    private static final String TAG = "TAG_LandMarkInfo";
+    private ImageButton mapInfoDetailBack, mapInfoDetailPlan;
+    private TextView mapInfoDetailTitle, mapInfoDetailAddress, mapInfoDetailDescription, mapInfoDetailOpenHours;
+    private ImageView mapInfoDetailPicture;
+    private RatingBar mapInfoDetailRatingBar;
+    private RecyclerView mapInfoDetailRecyclerView;
+    private String landMarkName;
+    private int imageSize;
+    private ImageTask landMarkImageTask;
+    private InfoImageTask infoImageTask;
+    private CommonTask imageIdTask, retrieveLocationTask;
+    private int imageId;
+    private Boolean imageIdCheck = false;
+    private List<LandMark> locations = null;
+    private List<Picture> pictures = null;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_land_mark_info);
+        imageSize = getResources().getDisplayMetrics().widthPixels / 3;
+        handleView();
+    }
+
+    public void handleView() {
+        mapInfoDetailBack = findViewById(R.id.mapInfoDetailBack);
+        mapInfoDetailPlan = findViewById(R.id.mapInfoDetailPlan);
+        mapInfoDetailTitle = findViewById(R.id.mapInfoDetailTitle);
+        mapInfoDetailAddress = findViewById(R.id.mapInfoDetailAddress);
+        mapInfoDetailDescription = findViewById(R.id.mapInfoDetailDescription);
+        mapInfoDetailOpenHours = findViewById(R.id.mapInfoDetailOpenHours);
+        mapInfoDetailPicture = findViewById(R.id.mapInfoDetailPicture);
+        mapInfoDetailRatingBar = findViewById(R.id.mapInfoDetailRatingBar);
+        mapInfoDetailRecyclerView = findViewById(R.id.mapInfoDetailRecyclerView);
+
+        //取得地標的名稱
+        Intent intent = getIntent();
+        landMarkName = intent.getStringExtra("landMarkName");
+
+        //返回上一頁
+        mapInfoDetailBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+
+        //加入行程
+        mapInfoDetailPlan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //跳到選行程？
+                Toast.makeText(LandMarkInfo.this, R.string.msg_NoNetwork, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        //用title＝landMark name 比對找出landMarkID 就可以知道imageID
+        if (Common.networkConnected(this)) {
+            String url = Common.URL + "/LocationServlet";
+            JsonObject jsonObject = new JsonObject();
+            jsonObject.addProperty("action", "findLocationId");
+            jsonObject.addProperty("name", landMarkName);
+            String jsonOut = jsonObject.toString();
+            imageIdTask = new CommonTask(url, jsonOut);
+            try {
+                String result = imageIdTask.execute().get();
+                imageId = Integer.valueOf(result);
+                imageIdCheck = true;
+            } catch (Exception e) {
+                Log.e(TAG, e.toString());
+            }
+        } else {
+            Common.showToast(this, R.string.msg_NoNetwork);
+            imageIdCheck = false;
+        }
+        //顯示圖片
+        if (imageIdCheck) {
+            String url = Common.URL + "/LocationServlet";
+            int id = imageId;
+            Bitmap bitmap = null;
+            try {
+                landMarkImageTask = new ImageTask(url, id, imageSize);
+                // passing null and calling get() means not to run FindImageByIdTask.onPostExecute()
+                bitmap = landMarkImageTask.execute().get();
+                imageIdCheck = false;
+            } catch (Exception e) {
+                Log.e(TAG, e.toString());
+            }
+            if (bitmap != null) {
+                mapInfoDetailPicture.setImageBitmap(bitmap);
+            } else {
+                mapInfoDetailPicture.setImageResource(R.drawable.default_image);
+                imageIdCheck = false;
+            }
+        }
+
+        //取得地標資訊
+        if (Common.networkConnected(this)) {
+            String url = Common.URL + "/LocationServlet";
+            JsonObject jsonObject = new JsonObject();
+            jsonObject.addProperty("action", "findLocationDetail");
+            jsonObject.addProperty("name", landMarkName);
+            //將內容轉成json字串
+            retrieveLocationTask = new CommonTask(url, jsonObject.toString());
+            try {
+                String jsonIn = retrieveLocationTask.execute().get();
+                Type listType = new TypeToken<List<LandMark>>() {
+                }.getType();
+                //解析 json to gson
+                locations = new Gson().fromJson(jsonIn, listType);
+            } catch (Exception e) {
+                Log.e(TAG, e.toString());
+            }
+            if (locations == null || locations.isEmpty()) {
+                Toast.makeText(this, R.string.msg_NoFoundLandMark, Toast.LENGTH_SHORT).show();
+            } else {
+                LandMark location = locations.get(0);
+                //顯示地標名稱
+                mapInfoDetailTitle.setText(location.getName());
+                //顯示地標地址
+                mapInfoDetailAddress.setText("Address : " + location.getAddress());
+                //顯示地標描述
+                mapInfoDetailDescription.setText("Description : " + location.getDescription());
+                //顯示地標開放時間
+                mapInfoDetailOpenHours.setText("Open Hours : " + location.getOpeningHours());
+
+                //找出地標裡所有照片id
+                if (Common.networkConnected(this)) {
+                    url = Common.URL + "/LocationServlet";
+                    jsonObject.addProperty("action", "findImageId");
+                    jsonObject.addProperty("id", location.getId());
+                    //將內容轉成json字串
+                    retrieveLocationTask = new CommonTask(url, jsonObject.toString());
+                    try {
+                        String jsonIn = retrieveLocationTask.execute().get();
+                        Type listType = new TypeToken<List<Picture>>() {
+                        }.getType();
+                        //解析 json to gson
+                        pictures = new Gson().fromJson(jsonIn, listType);
+                    } catch (Exception e) {
+                        Log.e(TAG, e.toString());
+                    }
+                    if (pictures == null || pictures.isEmpty()) {
+                        Toast.makeText(this, R.string.msg_NoFoundLandMark, Toast.LENGTH_SHORT).show();
+                    } else {
+                        //recycleView
+                        mapInfoDetailRecyclerView.setLayoutManager(
+                                new StaggeredGridLayoutManager(3,
+                                        StaggeredGridLayoutManager.VERTICAL));
+                        mapInfoDetailRecyclerView.setAdapter(new PictureAdapter(this, pictures));
+                    }
+                } else {
+                    Toast.makeText(this, R.string.msg_NoNetwork, Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        } else {
+            Toast.makeText(this, R.string.msg_NoNetwork, Toast.LENGTH_SHORT).show();
+        }
+
+
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        if (landMarkImageTask != null) {
+            landMarkImageTask.cancel(true);
+            landMarkImageTask = null;
+        }
+
+        if (imageIdTask != null) {
+            imageIdTask.cancel(true);
+            imageIdTask = null;
+        }
+
+        if (retrieveLocationTask != null) {
+            retrieveLocationTask.cancel(true);
+            retrieveLocationTask = null;
+        }
+
+        if (infoImageTask != null) {
+            infoImageTask.cancel(true);
+            infoImageTask = null;
+        }
+
+    }
+
+    private class PictureAdapter extends
+            RecyclerView.Adapter<PictureAdapter.MyViewHolder> {
+        private Context context;
+        private List<Picture> pictureList;
+
+        PictureAdapter(Context context, List<Picture> pictureList) {
+            this.context = context;
+            this.pictureList = pictureList;
+        }
+
+        class MyViewHolder extends RecyclerView.ViewHolder {
+            ImageView imageView;
+
+
+            MyViewHolder(View itemView) {
+                super(itemView);
+                imageView = itemView.findViewById(R.id.mapInfoDetailImageView);
+
+            }
+        }
+
+        @Override
+        public int getItemCount() {
+            return pictureList.size();
+        }
+
+
+        @NonNull
+        @Override
+        public MyViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int viewType) {
+            View itemView = LayoutInflater.from(context).
+                    inflate(R.layout.map_info_detail_image_item, viewGroup, false);
+            return new MyViewHolder(itemView);
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull MyViewHolder viewHolder, final int position) {
+            final Picture picture = pictureList.get(position);
+            String url = Common.URL + "/LocationServlet";
+            int id = picture.getImageID();
+            infoImageTask = new InfoImageTask(url, id, imageSize, viewHolder.imageView);
+            infoImageTask.execute();
+            final String text = String.valueOf(position);
+            viewHolder.imageView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    //點選照片跳到照片詳細內容
+                    Toast.makeText(LandMarkInfo.this, text, Toast.LENGTH_SHORT).show();
+
+                    Intent intent = new Intent(LandMarkInfo.this, LandMarkImageInfo.class);
+                    intent.putExtra("landMarkName",landMarkName);
+                    intent.putExtra("position",position);
+                    startActivity(intent);
+                }
+            });
+        }
+    }
+
+}
