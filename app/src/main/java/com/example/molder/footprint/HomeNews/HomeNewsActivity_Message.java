ca2faedbe20d1f6445ed1ae36739c95babce1b71
cockroach <weekend8394@gmail.com>
2019-02-10 16:42:18 +0800

    2/10

--- app/src/main/java/com/example/molder/footprint/HomeNews/HomeNewsActivity_Message.java
+++ app/src/main/java/com/example/molder/footprint/HomeNews/HomeNewsActivity_Message.java
@@ -9,42 +9,188 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.example.molder.footprint.Common.Common;
+import com.example.molder.footprint.Common.CommonTask;
+import com.example.molder.footprint.Home;
 import com.example.molder.footprint.R;
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import com.google.gson.reflect.TypeToken;
 
+import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.List;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
 public class HomeNewsActivity_Message extends AppCompatActivity {
-    private CircleImageView profile＿picture;
-    private TextView tv_news_userName;
+    private static String TAG = "HomeNewsActivity_Message";
+    private CircleImageView profile＿picture, personalUserHeadPicture,guestHeadPicture;
+    private TextView nickName, tv_description;
+    private Button sendButton;
+    private EditText commentMessage;
     private RecyclerView recyclerView;
+    private String userId, userNickName, description, personalUserId, message,guestId;
+    private CommonTask userIdTask, messageTask;
+    private HeadImageTask headImageTask;
+    private int imageSize, imageId;
+    private List<HomeNewsActivity_Message_Messages> newsMessage = null;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_news__message);
-        profile＿picture = findViewById(R.id.ci_profile＿picture);
-        tv_news_userName = findViewById(R.id.tv_news_userName);
+        imageSize = getResources().getDisplayMetrics().widthPixels;
         handleViews();
         showResults();
     }
 
     private void showResults() {
+        profile＿picture = findViewById(R.id.ci_profile＿picture);
+        nickName = findViewById(R.id.tv_news_userName);
+        tv_description = findViewById(R.id.tv_news_UserDescription);
+        sendButton = findViewById(R.id.bt_Comment);
+        commentMessage = findViewById(R.id.ti_Go_To_Comment);
+        personalUserHeadPicture = findViewById(R.id.ci_Comment_user);
+        //打開上一頁的打包的資料
         Bundle bundle = getIntent().getExtras();
         if (bundle != null) {
             HomeNewsFragment_News homeNewsFragmentNews = (HomeNewsFragment_News) bundle.getSerializable("news");
             if (homeNewsFragmentNews != null) {
-//                profile＿picture.setImageResource(homeNewsFragmentNews.getProfilePictureId());
-//                tv_news_userName.setText(homeNewsFragmentNews.getUserName());
+                userId = homeNewsFragmentNews.getUserID();
+                description = homeNewsFragmentNews.getDescription();
+                imageId = homeNewsFragmentNews.getImageID();
             }
+        }
+        //顯示照片描述
+        tv_description.setText(description);
+
+        //用userID 尋找使用者暱稱和頭像
+        if (Common.networkConnected(this)) {
+            String url = Common.URL + "/PicturesServlet";
+            JsonObject jsonObject = new JsonObject();
+            jsonObject.addProperty("action", "findUserNickName");
+            jsonObject.addProperty("id", userId);
+            userIdTask = new CommonTask(url, jsonObject.toString());
+            try {
+                //顯示使用者暱稱
+                String jsonIn = userIdTask.execute().get();
+                userNickName = String.valueOf(jsonIn);
+                nickName.setText(userNickName);
+
+                //使用者頭像
+                url = Common.URL + "/PicturesServlet";
+                headImageTask = new HeadImageTask(url, userId, imageSize, profile＿picture);
+                headImageTask.execute();
+
+            } catch (Exception e) {
+                Log.e(TAG, e.toString());
+            }
+        }
+
+        //取得目前登入使用者id
+        SharedPreferences preferences = getSharedPreferences(Common.PREF_FILE, MODE_PRIVATE);
+        personalUserId = preferences.getString("userId", "");
+
+        //用目前登入的使用者ID尋找使用者頭像
+        if (Common.networkConnected(this)) {
+            String url = Common.URL + "/PicturesServlet";
+            JsonObject jsonObject = new JsonObject();
+            jsonObject.addProperty("action", "findUserNickName");
+            jsonObject.addProperty("userId", personalUserId);
+            userIdTask = new CommonTask(url, jsonObject.toString());
+            try {
+                //使用者頭像
+                url = Common.URL + "/PicturesServlet";
+                headImageTask = new HeadImageTask(url, personalUserId, imageSize, personalUserHeadPicture);
+                headImageTask.execute();
+
+            } catch (Exception e) {
+                Log.e(TAG, e.toString());
+            }
+        }
+
+        //用照片id尋找所有留言
+        if (Common.networkConnected(this)) {
+            String url = Common.URL + "/CommentServlet";
+            JsonObject jsonObject = new JsonObject();
+            jsonObject.addProperty("action", "findAllMessage");
+            jsonObject.addProperty("imageId", imageId);
+            String jsonOut = jsonObject.toString();
+            messageTask = new CommonTask(url, jsonOut);
+            try {
+                String jsonIn = messageTask.execute().get();
+                Type listType = new TypeToken<List<HomeNewsActivity_Message_Messages>>() {
+                }.getType();
+                newsMessage = new Gson().fromJson(jsonIn, listType);
+
+            } catch (Exception e) {
+                Log.e(TAG, e.toString());
+            }
+            if (newsMessage == null || newsMessage.isEmpty()) {
+                Toast.makeText(this, R.string.msg_NoNewsFound, Toast.LENGTH_SHORT).show();
+            } else {
+                recyclerView.setAdapter(new HomeNews_MessageAdapter(this, newsMessage));
+            }
+        }
+
+        //留言按鍵的監聽器
+        sendButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (commentMessage == null) {
+                    // Toast.makeText(this, R.string.msg_NoMessage, Toast.LENGTH_SHORT).show();
+                }
+                if (personalUserId == null) {
+                    // Toast.makeText(this, R.string.msg_NoMessage, Toast.LENGTH_SHORT).show();
+                } else {
+                    insertMessage();
+                }
+            }
+        });
+    }
+
+    //上傳留言的方法
+    private void insertMessage() {
+        message = commentMessage.getText().toString().trim();
+        SharedPreferences preferences = getSharedPreferences(Common.PREF_FILE, MODE_PRIVATE);
+        personalUserId = preferences.getString("userId", "");
+        if (Common.networkConnected(this)) {
+            String url = Common.URL + "/CommentServlet";
+            HomeNewsActivity_Message_Messages messages;
+            messages = new HomeNewsActivity_Message_Messages(0, personalUserId, message, imageId);
+            JsonObject jsonObject = new JsonObject();
+            jsonObject.addProperty("action", "insert");
+            jsonObject.addProperty("share", new Gson().toJson(messages));
+            int count = 0;
+            try {
+                String result = new CommonTask(url, jsonObject.toString()).execute().get();
+                count = Integer.valueOf(result);
+                commentMessage.setText(null);
+//              Intent intent = new Intent(this, HomeNewsActivity_Message.class);
+//              startActivity(intent);
+            } catch (Exception e) {
+                Log.e(TAG, e.toString());
+            }
+            if (count == 0) {
+                Common.showToast(this, R.string.msg_InsertFail);
+            } else {
+                Common.showToast(this, R.string.msg_InsertSuccess);
+            }
+        } else {
+            Common.showToast(this, R.string.msg_NoNetwork);
         }
     }
 
-    private class HomeNews_MessageAdapter extends RecyclerView.Adapter {
+    private class HomeNews_MessageAdapter extends
+            RecyclerView.Adapter<HomeNews_MessageAdapter.MyViewHolder> {
         Context context;
         List<HomeNewsActivity_Message_Messages> newsMessage;
 
@@ -52,23 +198,61 @@ public class HomeNewsActivity_Message extends AppCompatActivity {
                                        List<HomeNewsActivity_Message_Messages> newsMessage) {
             this.context = context;
             this.newsMessage = newsMessage;
+            imageSize = getResources().getDisplayMetrics().widthPixels / 3; //getDisplayMetrics()取得目前螢幕
+
+        }
+
+        public class MyViewHolder extends RecyclerView.ViewHolder {
+            CircleImageView guestHeadPicture;
+            TextView tv_news_userName;
+            TextView tv_news_Comment;
+
+            public MyViewHolder(View itemView) {
+                super(itemView);
+                guestHeadPicture = itemView.findViewById(R.id.message_ci_profile＿picture);
+                tv_news_userName = itemView.findViewById(R.id.message_tv_news_userName);
+                tv_news_Comment = itemView.findViewById(R.id.message_tv_news_Comment);
+            }
         }
 
         @NonNull
         @Override
-        public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int viewType) {
+        public MyViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int viewType) {
             LayoutInflater layoutInflater = LayoutInflater.from(context);
             View itemView = layoutInflater.inflate(R.layout.activity_home_news__message_item, viewGroup, false);
             return new MyViewHolder(itemView);
         }
 
         @Override
-        public void onBindViewHolder(@NonNull RecyclerView.ViewHolder viewHolder, int i) {
+        public void onBindViewHolder(@NonNull MyViewHolder holder, int i) {
             final HomeNewsActivity_Message_Messages news_Message = newsMessage.get(i);
-            MyViewHolder myViewHolder = (MyViewHolder) viewHolder;
-            myViewHolder.circleImageView.setImageResource(news_Message.getCi_profile＿pictureId());
-            myViewHolder.tv_news_userName.setText(news_Message.getTv_news_userName());
-            myViewHolder.tv_news_Comment.setText(news_Message.getTv_news_Comment());
+            //顯示留言
+            holder.tv_news_Comment.setText(news_Message.getMessage());
+            guestId = news_Message.getUserId();
+
+            //用留言id的使用者id尋找暱稱和頭像
+            if (Common.networkConnected(HomeNewsActivity_Message.this)) {
+                String url = Common.URL + "/PicturesServlet";
+                JsonObject jsonObject = new JsonObject();
+                jsonObject.addProperty("action", "findUserNickName");
+                jsonObject.addProperty("id", guestId);
+                userIdTask = new CommonTask(url, jsonObject.toString());
+                try {
+                    //顯示使用者暱稱
+                    String jsonIn = userIdTask.execute().get();
+                    userNickName = String.valueOf(jsonIn);
+                    holder.tv_news_userName.setText(userNickName);
+
+                    //使用者頭像
+                    url = Common.URL + "/PicturesServlet";
+                    headImageTask = new HeadImageTask(url, guestId, imageSize, holder.guestHeadPicture);
+                    headImageTask.execute();
+
+                } catch (Exception e) {
+                    Log.e(TAG, e.toString());
+                }
+            }
+
         }
 
         @Override
@@ -77,57 +261,16 @@ public class HomeNewsActivity_Message extends AppCompatActivity {
         }
     }
 
-        private class MyViewHolder extends RecyclerView.ViewHolder {
-            CircleImageView circleImageView;
-            TextView tv_news_userName;
-            TextView tv_news_Comment;
-
-            public MyViewHolder(View itemView) {
-                super(itemView);
-                circleImageView = itemView.findViewById(R.id.message_ci_profile＿picture);
-                tv_news_userName = itemView.findViewById(R.id.message_tv_news_userName);
-                tv_news_Comment = itemView.findViewById(R.id.message_tv_news_Comment);
-            }
-        }
-
-        private void handleViews(){
-            recyclerView = findViewById(R.id.rv_home_news_message);
-            recyclerView.setLayoutManager(new LinearLayoutManager(this));//這邊輸入語法可以設定水平或垂直呈現
-            List<HomeNewsActivity_Message_Messages> newsMessageList = getNewsMessage();
-            recyclerView.setAdapter(new HomeNews_MessageAdapter(this, newsMessageList));
-        }
-
-        protected List<HomeNewsActivity_Message_Messages> getNewsMessage() {
-            List<HomeNewsActivity_Message_Messages> newsMessage = new ArrayList<>();
-            newsMessage.add(new HomeNewsActivity_Message_Messages
-                    (R.drawable.profile_picture_earth,"Earth","讓我們繼續看下去"));
-            newsMessage.add(new HomeNewsActivity_Message_Messages
-                    (R.drawable.profile_picture_cockroach,"Cockroach","讓我們繼續看下去+1"));
-            newsMessage.add(new HomeNewsActivity_Message_Messages
-                    (R.drawable.profile_picture_pinky,"Pinky","讓我們繼續看下去+1+1"));
-            newsMessage.add(new HomeNewsActivity_Message_Messages
-                    (R.drawable.com_facebook_profile_picture_blank_portrait,"None","讓我們繼續看下去+1+1+1"));
-            newsMessage.add(new HomeNewsActivity_Message_Messages
-                    (R.drawable.ic_facebook_logo,"Facebook","讓我們繼續看下去+1+1+1+1"));
-            newsMessage.add(new HomeNewsActivity_Message_Messages
-                    (R.drawable.ic_footprint_logo,"FootPrint","讓我們繼續看下去+1+1+1+1+1"));
-            newsMessage.add(new HomeNewsActivity_Message_Messages
-                    (R.drawable.default_image,"Hi","讓我們繼續看下去+1+1+1+1+1+1+1+1"));
-            newsMessage.add(new HomeNewsActivity_Message_Messages
-                    (R.drawable.profile_picture_earth,"Earth","讓我們繼續看下去"));
-            newsMessage.add(new HomeNewsActivity_Message_Messages
-                    (R.drawable.profile_picture_cockroach,"Cockroach","讓我們繼續看下去+1"));
-            newsMessage.add(new HomeNewsActivity_Message_Messages
-                    (R.drawable.profile_picture_pinky,"Pinky","讓我們繼續看下去+1+1"));
-            newsMessage.add(new HomeNewsActivity_Message_Messages
-                    (R.drawable.com_facebook_profile_picture_blank_portrait,"None","讓我們繼續看下去+1+1+1"));
-            newsMessage.add(new HomeNewsActivity_Message_Messages
-                    (R.drawable.ic_facebook_logo,"Facebook","讓我們繼續看下去+1+1+1+1"));
-            newsMessage.add(new HomeNewsActivity_Message_Messages
-                    (R.drawable.ic_footprint_logo,"FootPrint","讓我們繼續看下去+1+1+1+1+1"));
-            newsMessage.add(new HomeNewsActivity_Message_Messages
-                    (R.drawable.default_image,"Hi","讓我們繼續看下去+1+1+1+1+1+1+1+1"));
-            return newsMessage;
-        }
+    private void handleViews() {
+        recyclerView = findViewById(R.id.rv_home_news_message);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));//這邊輸入語法可以設定水平或垂直呈現
+        List<HomeNewsActivity_Message_Messages> newsMessageList = getNewsMessage();
+        recyclerView.setAdapter(new HomeNews_MessageAdapter(this, newsMessageList));
     }
+
+    protected List<HomeNewsActivity_Message_Messages> getNewsMessage() {
+        List<HomeNewsActivity_Message_Messages> newsMessage = new ArrayList<>();
+        return newsMessage;
+    }
+}
 
